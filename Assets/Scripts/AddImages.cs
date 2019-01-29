dc8eb46a7d143b2d76073d5adadeeab876150045
Paritosh <contact@iamparitoshsharma.me>
2019-01-30 03:47:05 +0530

    Initial Commit

---
+++ Assets/Scripts/AddImages.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddImages : MonoBehaviour
+{
+    public GameObject content;
+    // Use this for initialization
+    void Start()
+    {
+        object[] images;
+        for (int i = 0; i < 7; i++)
+        {
+            images = Resources.LoadAll("images/" + (i+1).ToString(), typeof(Sprite));
+            for (int j = 0;j < 3; j++)
+            {
+                Image img = content.transform.GetChild(i).GetChild(j).GetChild(1).gameObject.GetComponent<Image>();
+                img.sprite = (Sprite)images[j];
+                img.preserveAspect = true;
+            }            
+        }
+    }
+}
---
+++ Assets/Scripts/ChangeContent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ChangeContent : MonoBehaviour {
+
+    public void Next()
+    {
+        int i = EventSystem.current.currentSelectedGameObject.transform.parent.GetSiblingIndex();
+        i = (i + 1) % 3;
+        this.transform.parent.parent.GetChild(i).gameObject.SetActive(true);
+        this.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void Prev()
+    {
+        int i = this.transform.parent.GetSiblingIndex();
+        i = (Mathf.Abs(i - 1)) % 3;
+        this.transform.parent.parent.GetChild(i).gameObject.SetActive(true);
+        this.transform.parent.gameObject.SetActive(false);
+    }
+}
